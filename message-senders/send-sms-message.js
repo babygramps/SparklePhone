4e086ef866dd817d4c4ea147ecb813372f243f60
babygramps <pocolypz@gmail.com>
2019-04-04 14:40:53 -0700

    Added promise for status POST route

--- message-senders/send-sms-message.js
+++ message-senders/send-sms-message.js
@@ -1,27 +1,48 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-// Twilio set up
-
-// ES6 import and export isn't fully supported in node quite yet, currently it's experimental
-// import twilio from 'twilio';
 const twilio = require('twilio');
 const accountSid  = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
 const authToken   = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
 const twilioNumber = process.env.TWILIO_NUMBER; // From a valid Twilio number
 const client = new twilio(accountSid, authToken);
 
-// to is the number or email address of the recipient
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 function sendSmsMessage(user, messageBody) {
-  client.messages.create({
-    body: `${user.firstname}, there's a new announcement from SparklePhone: ${messageBody}`,
-    to: user.config,  // Text these numbers
-    from: twilioNumber
-  })
-  .then((message) => console.log(message.sid));
+     client.messages
+        .create({
+         body: messageBody,
+         from: twilioNumber,
+         statusCallback: process.env.TWILIO_STATUS_CALLBACK,
+         to: user.config
+       });
+
+       // create a new promise to return when Twilio POSTS the SMS status
+       return new Promise ((resolve, reject) => {
+         router.post('/', (req, res) => {
+           if (req.body.SmsStatus === 'delivered') {
+             resolve({
+               result: true,
+               method: 'phone',
+               user: req.body.To
+             })
+           } else if (req.body.SmsStatus !== 'sent') {
+            resolve({
+              result: false,
+              method: 'phone',
+              user: req.body.To
+            })
+           } res.sendStatus(200);
+         })
+       })
 }
 
-module.exports = sendSmsMessage;
-
-// ES6 import and export isn't fully supported in node quite yet, currently it's experimental
-// export default sendSmsMessage;
+module.exports = {
+  sendSmsMessage : sendSmsMessage,
+  router : router
+}
