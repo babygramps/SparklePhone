51f31f6ad48995f449d87fb68307833ff5d03918
babygramps <pocolypz@gmail.com>
2019-04-12 14:30:20 -0700

    clean up

--- useful-functions/filtering-functions.js
+++ useful-functions/filtering-functions.js
@@ -1,15 +1,50 @@
-const filteringFunctions = {
-// Create an array of users who are opted in
-    filterOptedInUsers: function (users){
-        const optedIn = users.filter((camper) => camper.optin === true);
-        return optedIn;
+let users = [
+    {
+        user: 'rick',
+        method: 'slack',
+        optin: true,
+        list: 'placement, space, something'
     },
+    {
+        user: 'james',
+        method: 'phone',
+        optin: false,
+        list: 'placement, space, blackhole'
+    },
+    {
+        user: 'bigboy',
+        method: 'email',
+        optin: true,
+        list: 'ketamine, placement'
+    },
+]
 
-    // Create an array of users by lists subscribed
-    filterByListSubscribed: function (users, listName){
-        const Subscribed = users.filter((user) => user.list === listName);
-     return Subscribed;
+users.map(user => user.list = user.list.split(', '))
+
+console.log(JSON.stringify(users))
+
+const filteringFunctions = {
+
+    filterByCriteria: function (users, criteria, condition){
+        return users.filter((camper) => camper[criteria] === condition);
+    },
+    
+    // Why can't includes find user[property]????
+    
+    filterByListName : function (array, property, listName){
+        return array.filter((user) => {
+            return user[property].includes(listName)
+        })
     }
+
 };
+
+let dock = filteringFunctions.filterByListName(users, 'list', 'space');
+
+const placement = filteringFunctions.filterByCriteria(dock, 'optin', true);
+// const optedIn = filteringFunctions.filterByCriteria(placement, 'optin', true);
+
+// const list = filteringFunctions.filteredIn(optedIn, 'list', 'ketamine')
+
 
 module.exports = filteringFunctions;
